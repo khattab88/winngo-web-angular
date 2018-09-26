a673ab8beb12eca353e201b6cbfbd8aa4f7cab18
Mahmoud Khattab <m_khatab_88@hotmail.com>
2018-09-26 11:11:50 +0200

    add firebase config to environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDY1A9oWJSsJsuXIRsbEOWfl5LmT3tUnqw",
+    authDomain: "winngo-6c09e.firebaseapp.com",
+    databaseURL: "https://winngo-6c09e.firebaseio.com",
+    projectId: "winngo-6c09e",
+    storageBucket: "",
+    messagingSenderId: "688394235086"
+  }
 };
