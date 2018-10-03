60f42066261af16f75bf9174706e01968776ff79
Mahmoud Khattab <m_khatab_88@hotmail.com>
2018-10-03 19:19:01 +0200

    fix brand images names

--- src/app/brands/brands.component.ts
+++ src/app/brands/brands.component.ts
@@ -12,31 +12,31 @@ export class BrandsComponent implements OnInit {
         id: 1,
         title: "Adidas",
         noOfBranches: 20,
-        img: "adidas-black-vector-logo.png"
+        img: "adidas.png"
     },
     {
         id: 2,
         title: "Zara",
         noOfBranches: 15,
-        img: "zara-logo.png"
+        img: "zara.png"
     },
     {
         id: 3,
         title: "Burger King",
         noOfBranches: 10,
-        img: "Burger_King_Logo.svg.png"
+        img: "burger-king.png"
     },
     {
         id: 4,
         title: "Smart Gym",
         noOfBranches: 12,
-        img: "gym-icon.png"
+        img: "gym-gym.png"
     },
     {
         id: 5,
         title: "StarBucks",
         noOfBranches: 16,
-        img: "starbucks-logo-png-transparent-0.png"
+        img: "starbucks.png"
     }
 ];
 
