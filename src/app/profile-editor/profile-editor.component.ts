044c706238e25ce459e25c3f5b0d12c254d69805
Mahmoud Khattab <m_khatab_88@hotmail.com>
2018-10-04 17:28:54 +0200

    implement profile editor

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Gender } from '../enums/gender';
+import { AuthService } from '../auth.service';
+import { UserService } from '../data/user.service';
 
 @Component({
   selector: 'profile-editor',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-editor.component.css']
 })
 export class ProfileEditorComponent implements OnInit {
+  name: string;
+  mobile: string;
+  email: string;
+  birthDate: Date;
+  gender: Gender;
+  homeArea: string;
+  workArea: string; 
 
-  constructor() { }
+  constructor(private userSvc: UserService, private auth: AuthService) { }
 
   ngOnInit() {
+    this.auth.user$.subscribe(u => {
+      this.userSvc.get(u.uid).subscribe(user => {
+        this.name = user.name;
+        this.mobile = user.mobile;
+        this.email = user.email;
+        this.gender = user.gender;
+        this.birthDate = user.birthDate;
+        this.homeArea = user.homeArea;
+        this.workArea = user.workArea;
+      });
+    });
   }
 
 }
